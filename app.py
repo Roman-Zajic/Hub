2d9e466e31c963b8fa324087c3db49539e73540e
Roman-Zajic <zajic.roman.mobile@gmail.com>
2026-07-16 06:43:03 +0200

    messages

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5001, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, port=5001, use_reloader=False, threaded=True)
